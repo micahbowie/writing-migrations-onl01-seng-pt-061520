9a0e6020708b73b0634acad189c6776fe74fd642
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 21:21:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column :artists do |t|
-      t.string :name
-    end 
+    change_column :students, 
   end 
 end
