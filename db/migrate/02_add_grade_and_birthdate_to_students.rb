c7187d032471e1236c0f80c15071d6ca235a2f19
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 21:19:57 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists do |t|
+    add_column :students, :grade, :Integer
       t.string :name
     end 
   end 
